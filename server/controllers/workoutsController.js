8fde5e66db3bf887960856b30c15f8e672903980
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-03-31 21:07:18 +0530

    Adding Schema for create the DB model

--- server/controllers/workoutsController.js
+++ server/controllers/workoutsController.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const allWorkoutsDocs = (req,res)=>{
     res.send('hello')
@@ -8,8 +9,14 @@ const getSingleDoc = (req,res)=>{
     res.send('hello')
 }
 
-const newWorkoutDoc = (req,res)=>{
-    res.send('hello')
+const newWorkoutDoc = async(req,res)=>{
+    const {title,load,reps} = req.body
+    try{
+        const workout = await Workout.create({title,load,reps})
+        res.status(200).json(workout)
+    }catch(error){
+        res.status(400).json({error:error.message})
+    }
 }
 
 const deleteWorkout = (req,res)=>{
