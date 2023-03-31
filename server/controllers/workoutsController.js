8b2b243a044a60756b2049a43ea5434c57b28ebc
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-03-31 22:10:28 +0530

    Completed Backend API for all functionality of my site with DB

--- server/controllers/workoutsController.js
+++ server/controllers/workoutsController.js
@@ -1,12 +1,21 @@
-const express = require('express')
 const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
 
-const allWorkoutsDocs = (req,res)=>{
-    res.send('hello')
+const allWorkoutsDocs = async(req,res)=>{
+    const workouts = await Workout.find({}).sort({createdAt : -1})
+    res.status(200).json(workouts)
 }
 
-const getSingleDoc = (req,res)=>{
-    res.send('hello')
+const getSingleDoc = async (req,res)=>{
+    const {id} = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(400).json({error:"No such workouts"})
+    }
+    const workout = await Workout.findById(id)
+    if (!workout){
+        res.status(400).json({error:"No such workouts"})
+    }
+    res.status(200).json(workout)
 }
 
 const newWorkoutDoc = async(req,res)=>{
@@ -19,12 +28,27 @@ const newWorkoutDoc = async(req,res)=>{
     }
 }
 
-const deleteWorkout = (req,res)=>{
-    res.send('hello')
+const deleteWorkout = async(req,res)=>{
+    const {id} = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(400).json({error:"No such workout"})
+    }
+    const workout = await Workout.findByIdAndDelete({_id:id})
+    if(!workout){
+        res.status(400).json({error:"No such workout"})
+    }
+    res.status(200).json(workout)
 }
 
-const updateWorkout = (req,res)=>{
-    res.send('hello')
+const updateWorkout = async(req,res)=>{
+    const {id} = req.params
+    const workout = await Workout.findOneAndUpdate({_id:id},{
+        ...req.body
+    })
+    if(!workout){
+        res.status(400).json({error:"No such workout"})
+    }
+    res.status(200).json(workout)
 }
 
 module.exports = {getSingleDoc,newWorkoutDoc,deleteWorkout,updateWorkout,allWorkoutsDocs}
