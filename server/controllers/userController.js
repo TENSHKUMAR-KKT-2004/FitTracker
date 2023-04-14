33e0f4a85a66e1d3686477bdb0dd51162c4856af
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-04-14 19:11:55 +0530

    generate token for the new users using JWT and send it to the client

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+// create jwt
+const createToken = (_id)=>{
+    return jwt.sign({_id},process.env.SECRET,{expiresIn: '3d'})
+}
 
 // login user
 
@@ -14,8 +20,10 @@ const signupUser = async (req,res)=>{
     const {email,password} = req.body
     try {
         const user = await User.signup(email,password)
+        
+        const token = createToken(user._id)
 
-        res.status(200).json(user)
+        res.status(200).json({email,token})
     }catch(error){
         res.status(400).json({error:error.message})
     }
