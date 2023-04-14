548bcc56d5ff74e6e2d4fdd44dcf1d4731bcfa7a
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-04-14 23:14:29 +0530

    create a custom hook for authenticate the new user and recive the token from server

--- FitTracker/src/pages/Signup.js
+++ FitTracker/src/pages/Signup.js
@@ -1,12 +1,16 @@
 import React,{useState} from 'react';
+import { useSignup } from '../hooks/useSignup';
+
 
 const Signup = () => {
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
+    const {signup,error,isLoading} = useSignup()
 
-    const handleSubmit = (e)=>{
+    const handleSubmit = async(e)=>{
         e.preventDefault()
-        console.log(email,password)
+        // to signup the user
+        await signup(email,password)
     }
 
     return ( 
@@ -24,7 +28,8 @@ const Signup = () => {
             onChange={(e)=>{setPassword(e.target.value)}}
             value={password}
              />
-             <button type='submit'>Sign up</button>
+             <button type='submit' disabled={isLoading}>Sign up</button>
+             {error && <div className="error">{error}</div>}
         </form>
      );
 }
