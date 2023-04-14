32213465862545d105aed78ea3ecfb5612544f37
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-04-14 17:05:44 +0530

    signup process and hashing the user password on static signup method on userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
 
 const Schema = mongoose.Schema
 
@@ -13,5 +15,24 @@ const userSchema = new Schema({
         required:true
     }
 })
+
+// static signup method
+userSchema.statics.signup = async function(email,password){
+    // check if the email already exist
+    const exist = await this.findOne({email})
+    if(exist){
+        throw Error('email already exist')
+    }
+
+    // gen the salt
+    const salt = await bcrypt.genSalt(10)
+    // hash the pass
+    const hash = await bcrypt.hash(password,salt)
+
+    // store the hashed pass and email
+    const user = this.create({email,password:hash})
+
+    return user
+}
 
 module.exports = mongoose.model('User',userSchema)
