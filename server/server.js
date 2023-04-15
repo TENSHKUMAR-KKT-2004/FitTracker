8fc219078c131ab3d826128adc5de1ad63e9d621
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-04-15 17:31:39 +0530

    Route all requests to the React app's index.html file for production

--- server/server.js
+++ server/server.js
@@ -2,17 +2,20 @@ require('dotenv').config()
 const { urlencoded } = require('express')
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
+
 // create server
 const app = express()
 
 const workoutsRoute = require('./routes/workoutsRoute')
 const userRoute = require('./routes/userRoute')
 
-
 // global middleware
 app.use(express.json())
 app.use(urlencoded({ extended: true }))
-app.use(express.static('build'))
+
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, 'build')))
 
 const DBURI = `mongodb+srv://${process.env.DBAuthName}:${process.env.DBAuthKey}@blog.dm0zbcz.mongodb.net/FitTrack`
 
@@ -29,6 +32,11 @@ app.use('/api/workouts', workoutsRoute)
 
 app.use('/api/user',userRoute)
 
+// Route all requests to the React app's index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
 app.use('',(req,res)=>{
-    res.status(404).send('URL not found ! :(')
+    res.status(404).send('URL not found !')
 })
