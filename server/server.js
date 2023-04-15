831138b30a9f095e662f947edfcd5c1da6b85809
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-04-15 15:51:18 +0530

    Build react app for production

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const userRoute = require('./routes/userRoute')
 // global middleware
 app.use(express.json())
 app.use(urlencoded({ extended: true }))
-
+app.use(express.static('build'))
 
 const DBURI = `mongodb+srv://${process.env.DBAuthName}:${process.env.DBAuthKey}@blog.dm0zbcz.mongodb.net/FitTrack`
 
